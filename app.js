081308bced479e6627bc3f2fde6a3ceab7071f8d
abbycrockett <arcrocs21@gmail.com>
2025-04-02 15:34:27 -0500

    Fix (Handle Larger Images)

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
